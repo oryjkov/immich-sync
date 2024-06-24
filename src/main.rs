8513eed6e7ebbfa157b94cd25f15390b8b3732f3
Oleg Ryjkov <oryjkov@gmail.com>
2024-06-24 16:04:04 +0200

    Using the page streaming library
    A few todos still left out but it somewhat works!

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
-use dotenvy::dotenv;
-use std::env;
-use std::fs;
-
+use anyhow::anyhow;
+use anyhow::Context;
+use futures::task::Poll;
+use futures_util::pin_mut;
+use gphotos_api::apis::default_api::ListAlbumsError;
+use gphotos_api::models::Album; //apis::mode default_api::list_albums(&self.api_config, Some(5), None).await?;
+use gphotos_api::models::ListAlbumsResponse;
 use openapi::apis::assets_api;
 use openapi::apis::configuration;
+use std::env;
+use std::fs;
+use std::future::Future;
+use std::pin::Pin;
+use std::time;
+use stream::*;
+use tokio_stream::{Stream, StreamExt};
 
 use oauth2::basic::BasicClient;
-use oauth2::{StandardTokenResponse, TokenResponse};
+use oauth2::{RefreshToken, StandardTokenResponse, TokenResponse};
 
 use oauth2::{ClientId, ClientSecret, TokenUrl};
 
@@ -22,18 +32,25 @@ struct SecretJs {
     client_secret: String,
 }
 
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    let auth_file = "auth_token.json";
-    let token: StandardTokenResponse<oauth2::EmptyExtraTokenFields, oauth2::basic::BasicTokenType> =
-        serde_json::from_str(&(fs::read_to_string(auth_file)?))?;
+struct AuthToken {
+    token: String,
+    expires_at: std::time::Instant,
+    refresh_token: String,
+}
+impl AuthToken {
+    fn new(refresh_token: &str) -> AuthToken {
+        AuthToken {
+            token: "".to_string(),
+            expires_at: time::Instant::now(),
+            refresh_token: refresh_token.to_string(),
+        }
+    }
 
-    let mut gp_api_config = gphotos_api::apis::configuration::Configuration {
-        oauth_access_token: Some(token.access_token().secret().clone()),
-        ..Default::default()
-    };
-    let mut r = gphotos_api::apis::default_api::list_albums(&gp_api_config, Some(50), None).await;
-    if r.is_err() {
+    async fn check_token(&mut self) -> anyhow::Result<()> {
+        if self.expires_at - time::Instant::now() > time::Duration::from_secs(60) {
+            return Ok(());
+        }
+
         let js = fs::read_to_string("client-secret.json")?;
         let secret_js = serde_json::from_str::<InstalledJs>(&js)?.installed;
 
@@ -45,40 +62,107 @@ async fn main() -> anyhow::Result<()> {
             // Following redirects opens the client up to SSRF vulnerabilities.
             .redirect(reqwest::redirect::Policy::none())
             .build()
-            .expect("Client should build");
+            .with_context(|| format!("Client should build"))?;
 
         let client = BasicClient::new(google_client_id)
             .set_token_uri(token_url)
             .set_client_secret(google_client_secret);
         let refresh_r = client
-            .exchange_refresh_token(token.refresh_token().unwrap())
+            .exchange_refresh_token(&RefreshToken::new(self.refresh_token.clone()))
             .request_async(&http_client)
-            .await;
+            .await
+            .with_context(|| format!("refresh token failed"))?;
         println!("refresh response: {:?}", refresh_r);
-        gp_api_config.oauth_access_token = Some(refresh_r.unwrap().access_token().secret().clone());
-        r = gphotos_api::apis::default_api::list_albums(&gp_api_config, Some(50), None).await;
+        self.token = refresh_r.access_token().secret().clone();
+        self.expires_at = time::Instant::now()
+            + refresh_r
+                .expires_in()
+                .unwrap_or(time::Duration::from_secs(3600));
+
+        Ok(())
     }
-    let r = r?;
+}
 
-    println!("got {} albums", r.albums.map_or(0, |a| a.len()));
+struct GPWrapper {
+    api_config: gphotos_api::apis::configuration::Configuration,
+}
 
-    dotenv().expect(".env file not found");
+// impl GPWrapper {
+//     async fn get_albums(&self) -> impl Stream<Item = anyhow::Result<Album>> {
+//         // TODO: not clone here
+//         let x = ResultsStream::new(self.api_config.clone());
+//
+//         x
+//     }
+// }
 
-    let mut api_config = configuration::Configuration::new();
-    api_config.api_key =
-        env::vars()
-            .find(|(k, _)| k == "API_KEY")
-            .map(|(_, v)| configuration::ApiKey {
-                prefix: None,
-                key: v,
-            });
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let auth_file = "auth_token.json";
+    // We only need the refresh token.
+    let saved_token: StandardTokenResponse<
+        oauth2::EmptyExtraTokenFields,
+        oauth2::basic::BasicTokenType,
+    > = serde_json::from_str(&(fs::read_to_string(auth_file)?))?;
+    let mut token = AuthToken::new(
+        saved_token
+            .refresh_token()
+            .ok_or(anyhow::anyhow!("can't find refresh token"))?
+            .secret(),
+    );
+    token.check_token().await?;
 
-    api_config.base_path = "http://h4:2283/api".to_string();
+    let gp_api_config = gphotos_api::apis::configuration::Configuration {
+        oauth_access_token: Some(token.token.clone()),
+        ..Default::default()
+    };
 
-    let r = assets_api::get_random(&api_config, Some(1.0))
-        .await
-        .unwrap();
-    println!("{:?}", r[0]);
+    let mut albums_stream = ResultsStream::new(|token: Option<String>| {
+        let c = gp_api_config.clone();
+        async move {
+            // TODO: how can I avoid this dance with converting String to str?
+            let r = if let Some(t) = token {
+                gphotos_api::apis::default_api::list_albums(&c, Some(5), Some(t.as_str())).await?
+            } else {
+                gphotos_api::apis::default_api::list_albums(&c, Some(5), None).await?
+            };
 
+            // TODO: seems like next token make it go in circles through the same albums!
+            match r.albums {
+                None => Ok::<
+                    Option<Page<String, gphotos_api::models::Album>>,
+                    gphotos_api::apis::Error<ListAlbumsError>,
+                >(None),
+                Some(albums) => {
+                    if albums.len() == 0 {
+                        Ok(None)
+                    } else {
+                        Ok(Some(Page::new(albums.into(), r.next_page_token)))
+                    }
+                }
+            }
+        }
+    });
+    while let Some(album) = albums_stream.next().await {
+        let album = album?;
+        println!("album {}", album.title.unwrap_or("no name".to_string()));
+    }
+
+    // let mut api_config = configuration::Configuration::new();
+    // api_config.api_key =
+    //     env::vars()
+    //         .find(|(k, _)| k == "API_KEY")
+    //         .map(|(_, v)| configuration::ApiKey {
+    //             prefix: None,
+    //             key: v,
+    //         });
+    //
+    // api_config.base_path = "http://h4:2283/api".to_string();
+    //
+    // let r = assets_api::get_random(&api_config, Some(1.0))
+    //     .await
+    //     .unwrap();
+    // println!("{:?}", r[0]);
+    //
     Ok(())
 }
