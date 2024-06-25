be977745d2484a7eee0cf02eda46ae17b4fa2193
Oleg Ryjkov <oryjkov@gmail.com>
2024-06-25 19:40:04 +0200

    remaining tables

--- mktable.sh
+++ mktable.sh
@@ -11,3 +11,26 @@ sqlite-utils create-table sqlite.db media_items \
     --not-null local_file \
     --pk=rowid \
     --strict
+
+sqlite-utils create-table sqlite.db albums \
+    rowid integer \
+    title text \
+    media_items_count integer \
+    share_info text \
+    id text \
+    cover_photo_media_item_id text \
+    --not-null id \
+    --pk=rowid \
+    --strict
+
+sqlite-utils create-table sqlite.db album_items \
+    album_id integer \
+    media_item_id integer \
+    --not-null album_id \
+    --not-null media_item_id \
+    --pk=album_id,media_item_id \
+    --strict
+
+sqlite-utils add-foreign-keys sqlite.db \
+    album_items album_id albums rowid \
+    album_items media_item_id media_items rowid
